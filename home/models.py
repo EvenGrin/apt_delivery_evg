e0b02bf179fc3cfea11417ff320256a44819d755
evelina <evelina.n.05@mail.ru>
2024-11-11 10:18:20 +0300

    init

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
+from symtable import Class
+
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Meal(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.FloatField()
+    out = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='images')
+    category = models.ForeignKey(Category, default=None, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from home import models
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    categories = models.Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'home/index.html', context)
