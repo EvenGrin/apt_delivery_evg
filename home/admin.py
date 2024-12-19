a1a6762f7ff13d424fb26ba32c85a4b3b088ffcc
evgen <evgeniagrigoreva67031@gmail.com>
2024-12-19 14:58:42 +0300

    админка

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
+from django.db.models import Q
+
 from home.models import Category, Meal, MenuDay
 
-
+@admin.register(Meal)
 class MealView(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'out', 'image', 'quantity')
-    list_filter = ('category', )
-
+    list_filter = ('category',)
+    search_fields = ('name__iregex',)  # Case-insensitive search directly
 # class MenuView(admin.ModelAdmin):
 #     list_display = ('date', 'meal')  # Add a custom display function
 
 
-
-
 admin.site.register(Category)
-admin.site.register(Meal, MealView)
 admin.site.register(MenuDay)
