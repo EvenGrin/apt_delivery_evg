28d06cbcced49e570cb34bc7e5aabf900b4b3b66
EvenGrin <evgeniagrigoreva67031@gmail.com>
2025-01-14 20:57:33 +0300

    Исправления

--- cart/admin.py
+++ cart/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from cart.models import Cart, Cabinet
 
-
+@admin.register(Cart)
 class CartView(admin.ModelAdmin):
     list_display = ('user', 'meal', 'quantity',)
 
-
+@admin.register(Cabinet)
 class CabView(admin.ModelAdmin):
     list_display = ('num', 'name',)
 
 
-admin.site.register(Cart, CartView)
-admin.site.register(Cabinet, CabView)
