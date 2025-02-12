3aa5211e85edc6f8dac9902da7f2ce61f3c51fab
EvenGrin <evgeniagrigoreva67031@gmail.com>
2025-02-12 23:06:54 +0300

    поиск

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Q
 from django.shortcuts import render
 
 from cart.models import Cart
@@ -6,7 +6,15 @@ from .models import Meal, Category
 
 
 def meal_list(request):
+    global search
+
     context = {}
+    if request.method == "GET":
+        context['search'] = request.GET.get('q')
+        search_result  = Meal.objects.filter(name__iregex=context['search'])
+        context['search_result'] = search_result
+        context['search_count'] = search_result.count()
+        context['search_count_all'] = Meal.objects.count()
     context['categories'] = Category.objects.prefetch_related(
         Prefetch(
             'meals',
@@ -14,6 +22,7 @@ def meal_list(request):
             to_attr='ordered_meals'
         )
     )
+
     if request.user.is_authenticated:
         context['cart_count'] = Cart.objects.filter(user=request.user).count()
         cart_items = Cart.objects.filter(user=request.user, meal__in=Meal.objects.all()).values('meal_id', 'quantity')
@@ -21,3 +30,4 @@ def meal_list(request):
         context['cart_items_id'] = [cart_item['meal_id'] for cart_item in cart_items]
 
     return render(request, 'home/index.html', context)
+
